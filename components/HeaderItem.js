ae31e975d18d13f1b1e6acd8004a95905e2d3143
Rakib544 <md.rakib10122003@gmail.com>
2021-06-19 10:11:20 +0600

    some code updated

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,7 +1,7 @@
 const HeaderItem = ({ title, Icon }) => {
     return (
         <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
-            <Icon className="h-8 mb-1 hover:animate-bounce"/>
+            <Icon className="h-8 mb-1"/>
             <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
         </div>
     );
